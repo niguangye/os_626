acfe61c88fe1db94a441e7f77bca5a4eee9cbba7
倪广野 <1542850280@qq.com>
2020-07-12 18:33:51 +0800

    [FEATURE](main.rs): 在panic方法中调用println!

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,13 @@
 mod vga_buffer;
 
 use core::panic::PanicInfo;
-use crate::vga_buffer::print_something;
 
 #[no_mangle] // 禁用名称重整，确保Rust编译器输出一个名为_start的函数；否则编译器可能最终生成名为_ZN3blog_os4_start7hb173fedf945531caE的函数，无法让链接器正确辨别。
 // extern "C" 告诉编译器这个函数应当使用C语言的调用约定
 pub extern "C" fn _start() -> ! {
 
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
@@ -21,6 +19,7 @@ pub extern "C" fn _start() -> ! {
 /// 这个函数从不返回，所以他被标记为发散函数（diverging function）。发散函数的返回类型称作Never类型（"never" type），记为!
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
