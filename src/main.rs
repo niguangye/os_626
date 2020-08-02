a9ad56ea10d42b28c4c79886816583cc7c7f91d3
倪广野 <1542850280@qq.com>
2020-08-02 16:47:56 +0800

    [REFACTOR](src/lib.rs,main.rs,basic_root.rs): 重构集成测试模块

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![no_std] // 禁用标准库链接
 #![no_main] // 告诉Rust编译器我们不使用预定义的入口点
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os_626::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
+use os_626::println;
 
 #[no_mangle] // 禁用名称重整，确保Rust编译器输出一个名为_start的函数；否则编译器可能最终生成名为_ZN3blog_os4_start7hb173fedf945531caE的函数，无法让链接器正确辨别。
 // extern "C" 告诉编译器这个函数应当使用C语言的调用约定
@@ -24,46 +22,20 @@ pub extern "C" fn _start() -> ! {
 /// 这个函数将在panic时被调用
 /// 类型为PanicInfo的参数包含了panic发生的文件名、代码行数和可选的错误信息
 /// 这个函数从不返回，所以他被标记为发散函数（diverging function）。发散函数的返回类型称作Never类型（"never" type），记为!
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", _info);
-    exit_qemu(QemuExitCode::Failed);
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os_626::test_panic_handler(info)
 }
 
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
-}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
 
 
 
--- tests/basic_root.rs
+++ tests/basic_root.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os_626::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use os_626::{println, serial_print, serial_println};
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -14,10 +15,18 @@ pub extern "C" fn _start() -> ! {
 }
 
 fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
     unimplemented!();
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    os_626::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
