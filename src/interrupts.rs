98f224b92a273e8ab0f46f386ece00229efa09db
倪广野 <1542850280@qq.com>
2020-08-23 16:00:27 +0800

    [TEST](interrupts.rs,lib.rs): 增加断点异常的测试

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -20,3 +20,12 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFra
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
+#[test_case]
+fn test_breakpoint_exception() {
+    serial_print!("test_breakpoint_exception... ");
+    x86_64::instructions::interrupts::int3();
+    serial_println!("[OK]");
+}
