7629c93182627c118a45ca9f4c37dae7db832cb5
倪广野 <1542850280@qq.com>
2020-09-30 12:19:42 +0800

    [FEATURE](main.rs,memory.rs): 添加创建映射函数create_example_mapping

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     use os_626::memory;
-    use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
+    use x86_64::{structures::paging::Page, VirtAddr};
 
     println!("Hello World{}", "!");
 
@@ -21,24 +21,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
 
-    let mapper = unsafe { memory::init(phys_mem_offset)};
+    let mut mapper = unsafe { memory::init(phys_mem_offset)};
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { memory::translate_addr(virt, phys_mem_offset) };
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let page = Page::containing_address(VirtAddr::new(0));
+
+    memory::create_example_mapping(page,&mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)}; // 0x_f021_f077_f065_f04e代表字符串 New！
 
     #[cfg(test)]
     test_main();
--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,18 @@
-use x86_64::{structures::paging::{ PageTable, OffsetPageTable}, VirtAddr, PhysAddr};
+use x86_64::{
+    structures::paging::{
+        PageTable, OffsetPageTable, Page, PhysFrame, Mapper, Size4KiB, FrameAllocator,
+    },
+    VirtAddr,
+    PhysAddr
+};
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
@@ -65,4 +79,21 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr)
 
     // calculate the physical address by adding the page offset
     Some(frame.start_address() + u64::from(addr.page_offset()))
+}
+
+// 创建从虚拟内存页到物理内存帧`0xb8000`的映射
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
 }
